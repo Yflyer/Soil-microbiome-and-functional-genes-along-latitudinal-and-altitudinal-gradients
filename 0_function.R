8505759a80515cb480b1929ed8ce20d7597e377b
Yflyer <zengyf93@qq.com>
2020-12-22 22:36:24 +0800

    R scripts for multi-gradients project

---
+++ 0_function.R
@@ -0,0 +1,175 @@
+########## 0_rawdata_process ######################
+# required package
+#library(ieggr)
+library(ggplot2)
+library(dplyr)
+library(vegan)
+library(geosphere)
+library(picante)
+library(dplyr)
+library(phyloseq)
+### clean NA and cut data by group
+clean_cut <- function(dt,map,group,cut=0.999){
+  group = map[[group]] %>% as.data.frame(.,row.names=rownames(map))
+  dt = dt %>% as.data.frame(.) %>% select_if(is.numeric) 
+  dt[is.na(dt)]=0
+  cut_result=geochip.trans(input=dt,grouping = group,note.col=0,cut = cut,cut.cross = 'treat',scaling = NA,trans = NA,save.wd=NULL)  
+  dt = cut_result$com %>% as.data.frame(.)
+}
+
+############ function used #############################
+group_count<-function(table,group){
+  ### this function is used to count the occurrence frequency of each row in a table in a specific grouping condition
+  ### such as: group_count(OTU,treatment)
+  ### then we will get each OTU occurrence assigned on treatment levels on rows
+  group= group %>% as.factor(.) %>% droplevels(.) # make factor
+  count_table = matrix(ncol= nlevels(group),nrow = nrow(table),dimnames = list(rownames(table),levels(group)))
+  for (i in 1:ncol(count_table)) {
+    dt = table[,group == colnames(count_table)[i]]
+    count_table[,i] = apply(dt, 1, function(x) sum(x>0))
+  }
+  count_table
+}
+
+counts_check = function(data_row,factor,level_counts=3,factor_counts=1){
+  # this will return the boolean list of  qualified level along data_row which meets requirement.
+  factor= factor %>% as.factor(.) %>% droplevels(.) # make factor
+  level_check_list = sapply(factor,function(x) data_row[,factor == x]>level_counts) 
+  sum(level_check_list)>factor_counts # this will print whether data row meets requirement of level_counts and factor_counts.
+}
+group_cut<-function(table,group,level_counts=3,group_counts=1){
+  ### this function is used to filter row data when you group count data table to meet requirement of group count frequency
+  ### such as: group_cut(OTU,treatment,level_counts=3,group_counts=1) the otu occured at least 3 times in a level of treatment will be filtered into new datatable.
+  otu_check_list = apply(table, 2, function(data_row) counts_check(data_row=data_row,factor=group,level_counts=level_counts,factor_counts=group_counts))
+  #table[otu_check_list,]
+  otu_check_list
+}
+
+group_sum<- function(table,group){
+  group= group %>% as.factor(.) %>% droplevels(.)
+  dt = matrix(ncol= nlevels(group),nrow = nrow(table),
+              dimnames = list(rownames(table),levels(group))) %>% as.data.frame(.)
+  for (i in levels(group)) {
+    dt[[i]] = apply(table,1,function(x) sum(x[group==i])) %>% as.numeric(.)
+  }
+  dt
+}
+
+group_mean<- function(table,group){
+  group= group %>% as.factor(.) %>% droplevels(.)
+  dt = matrix(ncol= nlevels(group),nrow = nrow(table),
+              dimnames = list(rownames(table),levels(group))) %>% as.data.frame(.)
+  for (i in levels(group)) {
+    dt[[i]] = apply(table,1,function(x) sum(x[group==i]))/summary(group)[i]
+  }
+  dt
+}
+
+###### get three column from dist martix
+col3m<-function(m,dist_name = 'dist'){
+  ###
+  m = as.matrix(m)
+  pair = data.frame(row=rownames(m)[row(m)[upper.tri(m,diag = F)]], 
+                    col=colnames(m)[col(m)[upper.tri(m,diag = F)]], 
+                    dist = m[upper.tri(m)])
+  colnames(pair)[3]=dist_name
+  pair
+}
+
+###### calculate the geospatial distance by lat and lon; need package: dplyr, geoshpere
+geo_dist = function(dis,Lon='Lon',Lat='Lat',unit = 'km'){
+  dis = dis[,c(Lon,Lat)] %>% as.matrix(.)
+  dis = apply(dis, 1, function(y) apply(dis, 1, function(x) distHaversine(y,x)))
+  if (unit == 'km'){dis/1000}
+}
+
+###### label the pair by factor
+label_pair = function(dt,meta,row='row',col='col',factor,sample){
+  # for diagonal-pair data to add pair tag
+  factor_row = sapply(dt[[row]], function(x) meta[[factor]][which(sample==x)])
+  factor_col = sapply(dt[[col]], function(x) meta[[factor]][which(sample==x)])
+  dt$pair=paste(factor_row,'-',factor_col)
+}
+
+######### stat tools
+get_aov_cof = function(formula,dt=NULL,cof='Pr(>F)',var=1){
+  # var: string or number, to locate the cof of var in the dataframe
+  # y : numeric vector
+  # x : treat
+  # waiting for extend to be complex formula
+  result = aov(as.formula(formula),data = dt) %>% summary(.)
+  result[[1]][var,cof]
+}
+
+get_mantel = function(var_list,var_name,Env_dt,tar,dist.type='euclid'){
+  # var_list = list or vector of the var you want to test or partially test
+  # var_name = name of the vars or category to name the dt
+  # Env_dt = all the env numeric data for mantel
+  # tar = the dist of OTU, etc, bray
+  # dist.type = env dist type
+  mantel.R = mantel.P = p.mantel.R =p.mantel.P = c()
+  for (i in 1:length(var_list)) {
+    var = unlist(var_list[i])
+    tar.dist = tar
+    var.dist = Env_dt[,var] %>% scale(.) %>% vegdist(.,dist.type)
+    ctr.dist = Env_dt[,!(all %in% var)] %>% scale(.) %>% vegdist(.,dist.type)
+    all.dist = Env_dt[,all] %>% scale(.) %>% vegdist(.,dist.type)
+    mantel=mantel(tar.dist,var.dist)
+    mantel.R[i] = mantel$statistic
+    mantel.P[i] = mantel$signif
+    p.mantel =  mantel.partial(tar.dist,all.dist,ctr.dist)
+    p.mantel.R[i] = p.mantel$statistic
+    p.mantel.P[i] = p.mantel$signif
+  }
+  mantel = data.frame(var_name,mantel.R,mantel.P,p.mantel.R,p.mantel.P)
+}
+
+#################
+get_var_name <- function(var) {
+  deparse(substitute(var))
+}
+
+labelp <- function(x){
+  if (x <= 0.05){x = "p < 0.05"} 
+  else if (x <= 0.1){x = "0.05 < p < 0.1"} 
+  else {x = "p > 0.1"}
+  x = factor(x,levels=c('p > 0.1','0.05 < p < 0.1','p < 0.05'))
+  }
+labelpstar <- function(x){
+  if (x <= 0.001){x = "***"}
+  else if (x <= 0.01){x = "**"}
+  else if (x <= 0.05){x = "*"} 
+  else if (x <= 0.1){x = "."}
+  else {x = ""}
+}
+
+labelpH <- function(x){
+  if (x < 5){x = "4 - 5"} 
+  else if (x < 6){x = "5 - 6"} 
+  else {x = "6 - 7"}
+  x = factor(x,levels=c('4 - 5','5 - 6','6 - 7'))
+}
+
+labelalt <- function(x){
+  if (x < 1000){x = "0-1000m"} 
+  else {x = "1000-1300m"}
+  x = factor(x,levels=c('0-1000m','1000-1300m'))
+}
+label_ln_dis <- function(x){
+  if (x < 6){x = "Small-scale (<150m)"} 
+  else {x = "Mid-scale (100-500km)"}
+  x = factor(x,levels=c('Small-scale (<150m)','Mid-scale (100-500km)'))
+}
+
+
+############
+writeOutput.F <- function(output.title, modelname) {
+  sink(file="modeloutputs.txt", append=TRUE) #creates an empty file; if TRUE, it will append to the file
+  print("##########################", row.names=FALSE)
+  print(output.title, row.names=FALSE)
+  print("##########################",  row.names=FALSE)
+  summary(modelname) #write summary of the model to text file 
+}
+
+
+
---
+++ 2_Decay_GeoChip.R
@@ -0,0 +1,133 @@
+library(geosphere)
+source('0_function.R')
+set.seed(999)
+
+load('project_HA_GeoChip')
+load('project_NA_GeoChip')
+load('project_SNJ_GeoChip')
+
+#################### save dir #######################
+folder = paste0('2_Decay_',note)
+dir.create(folder)
+
+##############################################################
+#################### Altitude (SNNR)
+prefix='SNNR'
+project = project_SNJ_GeoChip
+OTU <- otu_table(project)
+ENV <- sample_data(project) #%>% as.data.frame(.) 
+
+#################### 
+jaccard = vegdist(t(OTU), method="jaccard", binary=FALSE) %>% col3m(.,dist_name ='jaccard')
+bray = vegdist(t(OTU), method="bray", binary=FALSE) %>% col3m(.,dist_name ='bray')
+sorensen = vegdist(t(OTU), method="bray", binary=TRUE) %>% col3m(.,dist_name ='sorensen')
+
+distance = geo_dist(ENV) %>% col3m(.,dist_name = 'distance')
+#################### elevation distance
+distance.ele = ENV[,'Elevation'] %>% dist(.)/1000
+distance.ele = col3m(distance.ele,dist_name = 'distance') 
+distance$distance=distance$distance+distance.ele$distance
+#################### 
+decay_data = Reduce(function(x, y) inner_join(x, y,by=c('row','col')),list(distance,jaccard,bray,sorensen))
+
+#################### decay model examniation
+decay_data$Similarity.bray=1-decay_data$bray
+decay_data$ln.distance = log1p(decay_data$distance)
+model.result = betapart::decay.model(decay_data$Similarity.bray,decay_data$ln.distance,perm=999)
+model.result$p.value
+# snj: slope -0.1938805 p 0.5793936 p.p 0.6823177 p.b 0.3316683
+#################################################################
+decay_data$Study = 'Altitude (SNNR)'
+assign(paste0('decay_data_',prefix),decay_data)
+
+#################################################################
+#################### Altitude (HKV)
+prefix='HKV'
+project = project_HA_GeoChip #%>% subset_samples(.,! Site %in% c('C','D','E','F'))
+OTU <- otu_table(project)
+ENV <- sample_data(project) #%>% as.data.frame(.) 
+#################### 
+jaccard = vegdist(t(OTU), method="jaccard", binary=FALSE) %>% col3m(.,dist_name ='jaccard')
+bray = vegdist(t(OTU), method="bray", binary=FALSE) %>% col3m(.,dist_name ='bray')
+sorensen = vegdist(t(OTU), method="bray", binary=TRUE) %>% col3m(.,dist_name ='sorensen')
+
+distance = geo_dist(ENV) %>% col3m(.,dist_name = 'distance')
+#################### elevation distance
+distance.ele = ENV[,'Elevation'] %>% dist(.)/1000
+distance.ele = col3m(distance.ele,dist_name = 'distance') 
+distance$distance=distance$distance+distance.ele$distance
+#################### 
+decay_data = Reduce(function(x, y) inner_join(x, y,by=c('row','col')),list(distance,jaccard,bray,sorensen))
+#################### decay model examniation
+decay_data$Similarity.bray=1-decay_data$bray
+decay_data$ln.distance = log1p(decay_data$distance)
+model.result = betapart::decay.model(decay_data$Similarity.bray,decay_data$ln.distance,perm=999)
+model.result$p.value
+# ha: slope -0.1283425 p =0.008,P.PERM = 0.313 p.b = 0.174
+####################################################
+decay_data$Study = 'Altitude (HKV)'
+assign(paste0('decay_data_',prefix),decay_data)
+
+#################################################################
+#################### Latitude (NA)
+prefix='NA'
+project = project_NA_GeoChip
+OTU <- otu_table(project)
+ENV <- sample_data(project) #%>% as.data.frame(.) 
+#################### 
+jaccard = vegdist(t(OTU), method="jaccard", binary=FALSE) %>% col3m(.,dist_name ='jaccard')
+bray = vegdist(t(OTU), method="bray", binary=FALSE) %>% col3m(.,dist_name ='bray')
+sorensen = vegdist(t(OTU), method="bray", binary=TRUE) %>% col3m(.,dist_name ='sorensen')
+
+distance = geo_dist(ENV) %>% col3m(.,dist_name = 'distance')
+#################### site-inner distance (for latitudinal dataset)
+inner_distance = data.frame()
+for (i in 1:nlevels(ENV$Site)) {
+  dist = ENV[ENV$Site==levels(ENV$Site)[i],c('X','Y')] %>% dist(.)/1000 
+  dist = col3m(dist,dist_name = 'inner_distance')
+  inner_distance=rbind(inner_distance,dist)
+}
+inner_distance = left_join(distance,inner_distance,by=c('row','col'))
+inner_distance[is.na(inner_distance)]=0
+distance$distance=inner_distance$distance+inner_distance$inner_distance
+#################### 
+decay_data = Reduce(function(x, y) inner_join(x, y,by=c('row','col')),list(distance,jaccard,bray,sorensen))
+#################### decay model examniation
+decay_data$Similarity.bray=1-decay_data$bray
+decay_data$ln.distance = log1p(decay_data$distance)
+model.result = betapart::decay.model(decay_data$Similarity.bray,decay_data$ln.distance,perm=999)
+model.result$p.value
+# na: slope -0.0847966 p <0.001 p.p 0.012 p.b 0.003 p 0.0002408902
+####################################################
+decay_data$Study = 'Latitude (North America)'
+assign(paste0('decay_data_',prefix),decay_data)
+
+
+
+# plot
+PTs <- theme(#text = element_text(size=12),
+             #axis.text.x = element_text(size=16, hjust = 0.5,vjust = 0.5),
+             panel.background=element_blank(),
+             legend.position = "none",
+             panel.border = element_rect(colour = "grey", fill=NA, size=1))
+
+decay_data = Reduce(function(x, y) rbind(x,y),list(decay_data_HKV,decay_data_NA,decay_data_SNNR))
+
+decay_data$Study = factor(decay_data$Study,levels = c('Latitude (North America)','Altitude (SNNR)','Altitude (HKV)'))
+
+# log.distance
+SDecay <- ggplot(decay_data,aes(x=ln.distance,y=Similarity.bray))+
+  geom_point(alpha = 0.5,stroke = 0.3,size=1.5,shape=21,aes(fill=Study))+geom_smooth(method='lm',aes(color=Study),se = F,linetype =2)
+SDecay <- ggplot(decay_data,aes(x=ln.distance,y=Similarity.bray))+
+  geom_point(alpha = 0.5,stroke = 0.3,size=1.5,shape=21,aes(fill=Study))#+geom_smooth(aes(color=Study),se = F)
+SDecay+PTs+ylim(c(0.4,1))+facet_grid(~Study,scales = 'free')#+lm_Site+Ps_Sitescm_Site+sfm_Site
+ggsave(filename = paste0(folder,'/','3_Decay_',note,'_log_bray.pdf'),dpi=900,width=19.5,height=9,units='cm')
+ggsave(filename = paste0(folder,'/','3_Decay_',note,'_log_bray.jpg'),dpi=900,width=19.5,height=9,units='cm')
+
+# distance
+SDecay <- ggplot(decay_data,aes(x=distance,y=Similiarity_jaccard))+
+  geom_point(alpha = 0.5,stroke = 0.1,size=1,shape=21,aes(fill=Study))+geom_smooth(method='lm',aes(color=Study))
+SDecay+PTs+ylim(c(0,1))+facet_grid(~Study,scales = 'free')#+lm_Site+Ps_Sitescm_Site+sfm_Site
+ggsave(filename = paste0('3_Decay_',prefix,'_unlog_jaccard.pdf'),dpi=900,width=19.5,height=9,units='cm')
+
+
